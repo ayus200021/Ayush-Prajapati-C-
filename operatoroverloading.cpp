9e6221dde30e6842f0500d984dc42ed08063e1d7
ayus200021 <98148990+ayus200021@users.noreply.github.com>
2022-02-23 11:03:04 +0530

    Update operatoroverloading.cpp

--- operatoroverloading.cpp
+++ operatoroverloading.cpp
@@ -1,47 +1,63 @@
+//Khushi Mordani
 #include<iostream>
-
 using namespace std;
 
-class Complex{
-	
-	int real, img;
+class Complex
+{
+	float real,img;
 	public:
-	
-	Complex(){
-		real = 0;
-		img =0;
+		Complex() //default constructor
+		{
+			real = 0;
+			img = 0;
 		}
-	Complex(int x,int y){
-		real = x;
-		img  = y;
+		Complex(int a,int b) //Parameterized constructor
+		{
+			real = a;
+			img = b;
 		}
-	
-
-
-
-
-	void display(){
-		cout<<"The Value for real part :"<< real <<"& img is :"<< img<<endl;
+		void display()
+		{
+			cout<<"Real:"<<real<<" Imaginary:"<<img<<endl;
 		}
-
-	Complex operator +(Complex ob){
-		Complex temp;
-		temp.real = real + ob.real;
-		temp.img  = img  + ob.img;
-		return temp;
+		Complex operator +(Complex C) //addition operator overloading
+		{
+			Complex temp;
+			temp.real = real + C.real ;
+			temp.img = img + C.img;
+			return temp;
 		}
+		Complex operator -(Complex C) //subtraction operator overloading
+		{
+			Complex temp;
+			temp.real = real - C.real;
+			temp.img = img - C.img;
+			return temp;
+		}
+		Complex operator *(Complex C) //multiplication operator overloading
+		{
+			Complex temp;
+			temp.real = real * C.real;
+			temp.img = img * C.img;
+			return temp;
+		}
+		friend Complex operator /(Complex C1,Complex C2);
 };
+
 
 
 int main()
 {
-
-	Complex c1(1,1),c2(2,2);
-	Complex c3;
+	Complex c1(1,2),c2(3,4),c3;
 	c1.display();
 	c2.display();
-	
-    	c3=c1.operator+(c2);
 	c3.display();
-}
+	c3 = c1.operator +(c2);
+	c3.display();
+	c3 = c1.operator -(c2);
+	c3.display();
+	c3 = c1.operator *(c2);
+	c3.display();
 
+	return 0;
+}
